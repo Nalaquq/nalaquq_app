35e43d3eedc2a32aec11296d55d2190b583a0aa4
Nalkuq <seangleason52@gmail.com>
2022-06-20 15:17:43 -0400

    added splash screen and main to kivy app

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.video import Video
 from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivy.lang import Builder
 
+#Define different screens
+
+class SplashScreen(Screen): 
+    pass 
+
+class MainPage(Screen): 
+    pass
+
+class WindowManager(ScreenManager): 
+    pass 
+ 
 
 class AnchorLayoutExample(AnchorLayout):
     pass
-
 
 class BoxLayoutExample(BoxLayout):
     pass
